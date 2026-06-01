886ba85bc192825793d3094b7e1e4c71d773db84
Vanessa745 <vanessa.canaviri@ucb.edu.bo>
2026-06-01 12:44:54 -0400

    fix: expected más específicos

--- features/step_definitions/product_detailSteps.rb
+++ features/step_definitions/product_detailSteps.rb
@@ -30,5 +30,8 @@ Then('I return to the products page') do
   click_button 'Back to products'
 
   expect(page).to have_current_path('/inventory.html')
-  expect(page).to have_content('Products')
+  
+  expect(page).to have_css('.title', exact_text: 'Products')
+  expect(page).to have_css('.inventory_list')
+  expect(page).to have_css('.inventory_item', minimum: 1)
 end
